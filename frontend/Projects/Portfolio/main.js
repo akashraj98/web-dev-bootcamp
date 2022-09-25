3cb600e12dbe638d84bc656b58a77095deb0b582
akashraj98 <akashraj7713@gmail.com>
2022-09-25 10:31:54 +0530

    added local files

---
+++ frontend/Projects/Portfolio/main.js
@@ -0,0 +1,44 @@
+const dynamicContent = document.getElementById("dynamic-text");
+
+const phrases = ["Software Developer...","Learner...","Human Being..."];
+let phraseIndex = 0;
+let letterindex = 0;
+const typingSpeed = 100;
+const erasingSpeed = 150;
+
+function printLetters(phrase){
+    if (letterindex == phrase.length){
+        clearLetter();
+    }
+    else if (letterindex < phrase.length){
+        dynamicContent.textContent += phrase.charAt(letterindex)
+        console.log(phrase.charAt(letterindex))
+        letterindex+=1
+        // setTimeout(printLetters(phrase),300)   // # why this is incorrect?
+        setTimeout
+        setTimeout(function (){
+            printLetters(phrase) 
+        },typingSpeed)
+    }
+}
+function clearLetter(){
+    if (letterindex == -1){
+        phraseIndex = (phraseIndex +1) % phrases.length;
+        letterindex = 0;
+        printLetters(phrases[phraseIndex]);
+
+    }
+    else if (letterindex > -1){
+        let updatedPhrase = "";
+        for (index = 0; index < letterindex; index++){
+            updatedPhrase+=phrases[phraseIndex].charAt(index);
+        }
+        console.log(updatedPhrase)
+        dynamicContent.textContent = updatedPhrase;
+        letterindex -=1;
+        setTimeout(clearLetter,erasingSpeed); //difference between setInterval and set timeout
+
+    }
+
+}
+printLetters(phrases[phraseIndex])
